f8c5dd98a9026c89fc723c600394f540f82f73cf
kienntheprfs <kien.trantrung@hcmut.edu.vn>
2024-12-14 00:10:08 +0700

    fix

--- front-end/src/pages/employee-view/components/ProductActionButtons.jsx
+++ front-end/src/pages/employee-view/components/ProductActionButtons.jsx
@@ -4,10 +4,10 @@ import { Button } from "@/components/ui/button";
 const ProductActionButtons = () => {
   return (
     <div className="flex gap-4 mb-6">
-      <Button variant="ghost" className="hover:bg-[#8B9B7E]/10">All Product</Button>
+      {/* <Button variant="ghost" className="hover:bg-[#8B9B7E]/10">All Product</Button>
       <Button variant="ghost" className="hover:bg-[#8B9B7E]/10">Live</Button>
       <Button variant="ghost" className="hover:bg-[#8B9B7E]/10">Archive</Button>
-      <Button variant="ghost" className="hover:bg-[#8B9B7E]/10">Out of stock</Button>
+      <Button variant="ghost" className="hover:bg-[#8B9B7E]/10">Out of stock</Button> */}
     </div>
   );
 };
